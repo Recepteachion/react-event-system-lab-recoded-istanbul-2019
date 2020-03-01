05da304a75f7ff0114c6e84d41ea0a3b582cf847
Recep Öztürk <turkishteacher23@gmail.com>
2020-03-01 17:54:42 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -3,7 +3,10 @@ import React, {Component} from "react"
 
 class EyesOnMe extends Component{
   handleOnFocus =() =>{
-    
+    console.log("Good!")
+  }
+  handleOnBlur =() =>{
+    console.log("")
   }
   render(){
     return(
